069cfe6540baf2c0a3f3fbb400b86dca678e3cc5
John C. Lemme <jclemme@proportionallabs.com>
2016-11-23 12:39:42 -0500

    Definitions for v5 implementation plus doc cleanup

--- firmware/core/core_v5r0.v
+++ firmware/core/core_v5r0.v
@@ -34,7 +34,32 @@ module epRISC_core(iClk, iRst, oAddr, bData, oWrite, iMaskInt, iNonMaskInt, oHal
     output wire oFlg, oHalt;
     
     
-    always @(*) begin
-        
-        (rTmpA >> rTmpB) + ((rTmpA & (~(32'b1 << rTmpB))) << (32-rTmpB));
-    end
+    // System registers
+    reg [31:0] rRegIP, rRegSP, rRegCS, rRegGL;
+    
+    // Internal data registers
+    reg [31:0] rRegA, rRegB, rRegC, rRegS, rRegL;
+    reg [32:0] rRegR;
+    
+    // Internal control registers
+    reg [3:0] rPipeStatePrev, rPipeState, rPipeStateNext;
+    reg [31:0] rInst, rDataBuf;
+    
+    
+    // Instruction flags - worduck
+    wire mBranch, mBranchStandard, mBranchSaveStack, mBranchSaveLink, mBranchLoadStack, mBranchLoadLink;
+    wire mLoad, mLoadLoad, mLoadStore;
+    wire mDirect;
+    wire mALU, mALURegisters, mALUDirect;
+    wire mRegister, mRegisterStandard, mRegisterSwap;
+    wire mCore;
+    
+    // Instruction fields
+    wire fBranchCondition, fBranchBase, fBranchOffset;
+    wire fLoadBase, fLoadTarget, fLoadOffset;
+    wire fDirectDestination, fDirectShift, fDirectValue;
+    wire fALUOperation, fALUDestination, fALUTermA, fALUTermB, fALUShift, fALUValue;
+    wire fRegisterDestination, fRegisterSource;
+    wire fCoreOperation, fCoreData;
+
+
